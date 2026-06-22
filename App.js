cc9d574c616a1508962b60bca40103cba96e4ad3
Meghana <siddimeghana3@gmail.com>
2026-06-22 19:24:28 +0530

    empty repo before creating app

--- App.js
+++ App.js
@@ -1,23 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Title = () => (
-  <h1 id="heading" className="head ">
-    This is Title Component
-  </h1>
-);
-// Component Composition
-const HeaderComponent = () => {
-  return (
-    <div id="container">
-        <Title/>
-      <h1 className="heading"> React Functional Component </h1>
-    </div>
-  );
-};
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
+
 
 
 root.render(<HeaderComponent />);
